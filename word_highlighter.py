3aecc9e548db1784ffd81ddd617d7046726c1f2c
invander80 <82331428+invander80@users.noreply.github.com>
2021-10-10 18:40:43 +0200

    Update word_highlighter.py

--- word_highlighter.py
+++ word_highlighter.py
@@ -1,15 +1,15 @@
 import tkinter
-from tkinter import Text,Tk
+from tkinter import Text, Tk
 
 root = Tk()
 
 
-def highlighter(event):
-    length = len("Hello")
+def highlighter(char, event=None):
+    length = len(char)
     index = int(round(float(txt.index(tkinter.INSERT)), 0))
     val = len(txt.get("1.0", "end-1c"))
     for x in range(len(txt.get("1.0", "end-1c"))):
-        if "Hello" in txt.get(f"{index}.{x}", "end-1c") :
+        if char in txt.get(f"{index}.{x}", "end-1c") :
             val = x
             txt.tag_config("Hello", background=txt.cget("bg"), foreground="red")
     vals = str(val + length)
@@ -19,7 +19,7 @@ def highlighter(event):
 txt = Text(root)
 txt.grid(columnspan=3)
 
-root.bind("<Key>", highlighter)
+root.bind("<Key>", lambda event: highlighter("Hello"))
 
 
 root.mainloop()
